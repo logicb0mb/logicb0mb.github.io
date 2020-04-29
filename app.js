c92b88f71e9fffbc588ccf8ced39551cec9e2284
Shreyas Shukla <shreyas.shukla26@gmail.com>
2020-04-29 20:13:27 +0530

    Particles

---
+++ app.js
@@ -0,0 +1,5 @@
+function executeParticles() {
+    particlesJS.load('particles-js', 'assets/particlesjs.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
+}
